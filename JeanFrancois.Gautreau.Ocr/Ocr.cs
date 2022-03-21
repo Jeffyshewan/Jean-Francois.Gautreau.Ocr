20a1befe78e14b8cc10b4f27d031259d9f942c45
Jean-François GAUTREAU <Jeff59550@gmail.com>
2022-03-21 09:06:06 +0100

    :sparkles: : add ocr & ocrResult

--- JeanFrancois.Gautreau.Ocr/Ocr.cs
+++ JeanFrancois.Gautreau.Ocr/Ocr.cs
@@ -1,6 +1,43 @@
-﻿namespace JeanFrancois.Gautreau.Ocr;
+﻿using System.Reflection;
+using Tesseract;
+
+namespace JeanFrancois.Gautreau.Ocr;
 
 public class Ocr
 {
-    
+    public async Task<IList<OcrResult>> ReadTextInImage(IList<byte[]> images)
+    {
+        IList<OcrResult> ocrResults = new List<OcrResult>();
+        foreach (var image in images)
+        {
+            ocrResults.Add(await Task.Run(() => DetectTextInImage(image)));
+        }
+
+        return ocrResults;
+    }
+
+    private static string GetExecutingPath()
+    {
+        var executingAssemblyPath = Assembly.GetExecutingAssembly().Location;
+        var executingPath = Path.GetDirectoryName(executingAssemblyPath);
+
+        return executingPath;
+    }
+
+    public OcrResult DetectTextInImage(byte[] image)
+    {
+        var ocrResultList = new List<OcrResult>();
+        var executingAssemblyPath = Assembly.GetExecutingAssembly().Location;
+        var executingPath = Path.GetDirectoryName(executingAssemblyPath);
+
+        using var engine = new TesseractEngine(Path.Combine(executingPath, @"tessdata"), "fra", EngineMode.Default);
+        using var pix = Pix.LoadFromMemory(image);
+        var test = engine.Process(pix);
+
+        return new OcrResult()
+        {
+            Text = test.GetText(),
+            Confidence = test.GetMeanConfidence()
+        };
+    }
 }
---
+++ JeanFrancois.Gautreau.Ocr/OcrResult.cs
@@ -0,0 +1,7 @@
+﻿namespace JeanFrancois.Gautreau.Ocr;
+
+public class OcrResult
+{
+    public string Text { get; set; }
+    public float Confidence { get; set; }
+}
