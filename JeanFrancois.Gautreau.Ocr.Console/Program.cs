19df3ce765916ef70ae904f1aeb5ce6500bf23ca
Jean-François GAUTREAU <Jeff59550@gmail.com>
2022-03-21 09:27:53 +0100

    :sparkles: : add console program

---
+++ JeanFrancois.Gautreau.Ocr.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using JeanFrancois.Gautreau.Ocr;
+
+static class MyConsoleProgram
+{
+    static async Task Main(string[] args)
+    {
+
+        var imagesData = new List<byte[]>();
+        foreach (var imgPath in Directory.EnumerateFiles(args[0]))
+        {
+            var imgBytes = await File.ReadAllBytesAsync(imgPath);
+            imagesData.Add(imgBytes);
+        }
+
+        var ocr = new Ocr();
+        var ocrResults = await  ocr.ReadTextInImage(imagesData);
+        foreach (var ocrResult in ocrResults)
+        {
+            System.Console.WriteLine($"Confidence :{ocrResult.Confidence}");
+            System.Console.WriteLine($"Text :{ocrResult.Text}"); 
+        }
+    }
+}
